f0c0e1ca2c42cdf1c70a3f913912d642ef55aa70
Daniel Carlson <danieljeffcarlson@gmail.com>
2020-08-02 16:06:37 -0400

    Initial Commit

---
+++ tio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tio
+//
+//  Created by Charles A. Carlson on 8/2/20.
+//  Copyright © 2020 Daniel Carlson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tioTests/tioTests.swift
@@ -0,0 +1,34 @@
+//
+//  tioTests.swift
+//  tioTests
+//
+//  Created by Charles A. Carlson on 8/2/20.
+//  Copyright © 2020 Daniel Carlson. All rights reserved.
+//
+
+import XCTest
+@testable import tio
+
+class tioTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
